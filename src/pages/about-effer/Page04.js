a5fd85ebdf1f1bd2d05b38ee94e7b3fb73220fc5
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-07 11:32:43 +0200

    add between section page

--- src/pages/about-effer/Page04.js
+++ src/pages/about-effer/Page04.js
@@ -1,11 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
+// components
+import Navigation from "../../components/Navigation";
+import BackgroundImage from "../../components/styled/BackgroundImage";
+
+// images
+import Background from "../../assets/images/effer01.jpg";
+
+const StyledDiv = styled.div`
+  padding: ${props => props.theme.windowPadding};
+`;
 
 const Page04 = () => {
   return (
     <div className="page-transition">
-      <h1>Page04</h1>
-      <h2>About EFFER</h2>
+      <BackgroundImage image={Background}>
+        <StyledDiv>
+          <h1>NEW “BETWEEN SECTIONS” SLIDE TO CATCH ONE’S BREATH</h1>
+          <Navigation backTo="/about-effer/page03" to="/about-effer/page05"/>
+        </StyledDiv>
+      </BackgroundImage>
     </div>
   );
 }
