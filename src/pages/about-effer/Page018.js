e2d61327a2fc153be18ef0927366af88221adb44
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-15 10:51:05 +0200

    extreme video popup

--- src/pages/about-effer/Page018.js
+++ src/pages/about-effer/Page018.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 import BackgroundImage from "../../components/styled/BackgroundImage";
 import Navigation from "../../components/Navigation";
@@ -7,16 +8,23 @@ import RedArrowLink from "../../components/RedArrowLink";
 
 import EfferLogo from "../../assets/icons/effer-logo.svg";
 import Background from "../../assets/images/effer_hiab-background.jpg";
+import closeIcon from "../../assets/icons/close-menu.svg";
+
 
 const StyledDiv = styled.div`
-  height: 70vh;
+  height: 100vh;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   h2{
     margin: 0;
     padding-top: 20px;
   }
   h1{
     font-size: 66px;
+    letter-spacing: 20px;
     margin: 0;
     ${'' /* padding-bottom: 60px; */}
   }
@@ -25,44 +33,93 @@ const StyledDiv = styled.div`
     top: 20px;
     left: 30px;
   }
-  ${'' /* .video-container{
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding-bottom: 56.25%;
-    video{
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-    }
-  } */}
 
-  .RedArrowLink-wrapper{
-    display: inline;
-    position: absolute;
-    bottom: 50px;
-    width: 40%;
-    left: 30%;
+  .open-popup{
+    padding: 10px 20px;
+    font-weight:900;
+    font-size: 22px;
+    background:${props => props.theme.red}; 
+    color: #fff;
+    text-transform: uppercase;
+    transition: all .3s ease;
   }
+  .open-popup:hover{
+    background: #fff;
+    color: ${props => props.theme.red}; 
+    cursor: pointer;
+    transition: all .3s ease;
+  }
+
+  .compare{
+    font-size: 22px;
+    color: ${props => props.theme.red};
+    position: absolute;
+    bottom: 80px;
+    padding: 10px 20px;
+    font-weight: 900;
+    transition: all .3s ease;
+  }
+  .compare:hover{
+    background: ${props => props.theme.red};
+    color: #fff;
+    transition: all .3s ease;
+  }
+  .video-popup{
+    z-index: 110;
+    position: absolute;
+    top:0 ;
+    left : 0;
+    height: 100vh;
+    width: 100vw;
+    background: rgba(0,0,0, 0.8);
+    padding: 100px;
+    .close-icon{
+      position: absolute;
+      top: 40px;
+      right: 40px;
+      width: 40px;
+      height: 40px;
+      transition: all .2s ease;
+    }
+    .close-icon:hover{
+      cursor: pointer;
+      transform: scale(1.2);
+      transition: all .2s ease;
+    }
+    .video-popup-inner{
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      height: 100%;
+      .video {
+        background: #fff;
+        ${'' /* box-shadow: 1px 1px 12px #888; */}
+        border-radius: 8px;
+        padding: 10px;
+        position: absolute;
+        top: 50%;
+        right: 50%;
+        transform: translate(50%, -50%);
+        width: 70%;
+        height: 100%;
+      }
+    }
+  }
+
   @media ${props => props.theme.firstBreakpoint}{
     h2{
-      padding-top: 60px;
+    }
+    h1{
+      font-size: 100px;
     }
   }
   @media ${props => props.theme.secondBreakpoint}{
     h2{
-      padding-top: 80px;
-    }
-    .RedArrowLink-wrapper{
-      width: 30%;
-      left: 35%;
     }
   }
   @media ${props => props.theme.thirdBreakpoint}{
     h2{
-      padding-top: 120px;
     }
     .RedArrowLink-wrapper{
       width: 20%;
@@ -73,6 +130,9 @@ const StyledDiv = styled.div`
 
 
 const Page018 = props => {
+
+  const [toggle, setToggle] = useState(false);
+
   return(
     <div className="page-transition">
       <BackgroundImage image={Background}>
@@ -80,14 +140,21 @@ const Page018 = props => {
           <img className="logo" alt="effer logo" src={EfferLogo} />
           
           <h2>If we could boil it down to just one word, it would be…</h2>
-          <h1>EXTREME</h1>
-          <div className="video-container">
-            <iframe className="video" title="EXTREME" allowFullScreen width="560" height="315" src="https://www.youtube.com/embed/X3QfnmKL3yw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-          </div>
+          <h1>EXTREME</h1>        
 
-          <div className="RedArrowLink-wrapper">
-            <RedArrowLink text="Compare with HIAB" toWhere="dont now yet"/>
-          </div>
+          <p className="open-popup" onClick={ () => setToggle(!toggle)}>Play video</p>
+
+          {toggle ? 
+            <div className="video-popup" onClick={ () => setToggle(!toggle)}>
+              <div className="video-popup-inner">
+              {/* <iframe className="video" title="EXTREME" allowFullScreen width="560" height="315" src="https://www.youtube.com/embed/X3QfnmKL3yw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
+              <iframe className="video" title="EXTREME" allowFullScreen width="560" height="315" src="https://www.youtube.com/embed/sVkqRawzebo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+              </div>
+              <img className="close-icon" src={closeIcon} alt="close icon" onClick={ () => setToggle(!toggle)}/> 
+            </div>
+          : ""}
+
+          <Link className="compare" to="Compare with HIAB">Compare with HIAB</Link>
 
           <Navigation backTo="/about-effer/page017" to="/about-effer/page019"/>
         </StyledDiv>
