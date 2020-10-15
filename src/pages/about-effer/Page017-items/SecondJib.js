e49e617bc424151e5d22d6096922d43c0039565f
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-15 09:52:04 +0200

    second jib video popup

--- src/pages/about-effer/Page017-items/SecondJib.js
+++ src/pages/about-effer/Page017-items/SecondJib.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+
+import secondJib from "../../../assets/images/effer/secondJib.png";
+import playButton from "../../../assets/icons/play-button02.png";
+import closeIcon from "../../../assets/icons/close-menu.svg";
 
 const StyledDiv = styled.div`
   h1{
@@ -9,7 +13,7 @@ const StyledDiv = styled.div`
     max-width: 1340px;
     margin: 0 auto;
     display: grid;
-    grid-template-columns: 1fr 2fr;
+    grid-template-columns: 1fr;
     grid-gap: 20px;
     .left{
       h3{
@@ -21,15 +25,65 @@ const StyledDiv = styled.div`
     }
     .right{
       position: relative;
-      width: 100%;
-      height: 0;
-      padding-bottom: 56.25%;
-      .video {
-        position: absolute;
-        top: 0;
-        left: 0;
+      
+      img.video-image{
         width: 100%;
-        height: 100%;
+      }
+      img.video-image:hover{
+        opacity: 0.5;
+        cursor: pointer;
+      }
+      img.play-icon{
+        pointer-events: none;
+        width: 100px;
+        position: absolute;
+        top: 50%;
+        right: 50%;
+        transform: translate(50%, -50%);
+        background: transparent;
+
+      }
+    }
+  }
+
+  .video-popup{
+    z-index: 110;
+    position: absolute;
+    top:0 ;
+    left : 0;
+    height: 100vh;
+    width: 100vw;
+    background: rgba(0,0,0, 0.8);
+    padding: 100px;
+    .close-icon{
+      position: absolute;
+      top: 40px;
+      right: 40px;
+      width: 40px;
+      height: 40px;
+      transition: all .2s ease;
+    }
+    .close-icon:hover{
+      cursor: pointer;
+      transform: scale(1.2);
+      transition: all .2s ease;
+    }
+    .video-popup-inner{
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      height: 100%;
+      .video {
+        background: #fff;
+        border-radius: 8px;
+        padding: 10px;
+        position: absolute;
+        top: 50%;
+        right: 50%;
+        transform: translate(50%, -50%);
+        width: 80%;
+        height: 80%;
       }
     }
   }
@@ -40,6 +94,10 @@ const StyledDiv = styled.div`
     }
   }
   @media ${props => props.theme.secondBreakpoint}{
+    .grid{
+      padding: 0 80px;
+      grid-template-columns: 1fr 1fr;
+    }
     h1{
       font-size: 38px;
     }
@@ -53,6 +111,9 @@ const StyledDiv = styled.div`
 `;
 
 const ContinuouSlewing = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return(
     <StyledDiv>
       <h1>Second jib</h1>
@@ -60,9 +121,20 @@ const ContinuouSlewing = () => {
         <div className="left">
           <p>no text yet no text yet no text yet no text yet no text yet no text yet no text yet no text yet </p>
         </div>
-        <div className="right">
-          <iframe className="video" title="secondjib" allowFullScreen width="560" height="315" src="https://www.youtube.com/embed/wbNNpnZ7BuU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+
+        <div className="right" onClick={() => setToggle(!toggle)}>
+          <img className="video-image" src={secondJib}  alt="image"/>
+          <img className="play-icon" src={playButton} alt="play video"/>
         </div>
+
+        {toggle ? 
+          <div className="video-popup" onClick={ () => setToggle(!toggle)}>
+            <div className="video-popup-inner">
+            <iframe className="video" title="secondjib" allowFullScreen width="560" height="315" src="https://www.youtube.com/embed/wbNNpnZ7BuU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            </div>
+            <img className="close-icon" src={closeIcon} alt="close icon" onClick={ () => setToggle(!toggle)}/> 
+          </div>
+        : ""}
       </div>
     </StyledDiv>
   );
