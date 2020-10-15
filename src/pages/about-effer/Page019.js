f2bd5e72d4974ac61e843f9902f7b30a5b84e5b0
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-15 11:01:41 +0200

    change font size on effer 19

--- src/pages/about-effer/Page019.js
+++ src/pages/about-effer/Page019.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import SingleHeadingMiddle from "../../pagetemplates/SingleHeadingMiddle";
 import Navigation from "../../components/Navigation";
@@ -6,11 +7,33 @@ import Navigation from "../../components/Navigation";
 // images
 import Background from "../../assets/images/effer_hiab-background.jpg";
 
+const StyledDiv = styled.div`
+  h1{
+    font-size:26px;
+    width:600px;
+  }
+
+  @media ${props => props.theme.secondBreakpoint}{
+    h1{
+      font-size:42px;
+      width:800px;
+    }
+  }
+  @media ${props => props.theme.thirdBreakpoint}{
+    h1{
+      font-size:52px;
+      width: 1000px;
+    }
+  }
+`;
+
 const Page019 = () => {
   return (
-    <SingleHeadingMiddle overTitle="So… " title="How can this knowledge help you when dealing with customers?" backgroundImage={Background} showEfferLogo={true} >
-      <Navigation backTo="/about-effer/page018" to="/one-unbeatable-offer/page01"/>
-    </SingleHeadingMiddle>
+    <StyledDiv>
+      <SingleHeadingMiddle overTitle="So… " title="How can this knowledge help you when dealing with customers?" backgroundImage={Background} showEfferLogo={true} >
+        <Navigation backTo="/about-effer/page018" to="/one-unbeatable-offer/page01"/>
+      </SingleHeadingMiddle>
+    </StyledDiv>
   );
 }
 
