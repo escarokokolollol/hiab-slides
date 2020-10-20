2694b5396aef53e3f460eb99b1b54f56497476ca
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-20 17:13:05 +0200

    change hiab 01 background image

--- src/pages/about-hiab/Page01.js
+++ src/pages/about-hiab/Page01.js
@@ -8,7 +8,7 @@ import BackgroundImage from "../../components/styled/BackgroundImage";
 import InfoWrapper from "../../components/InfoWrapper";
 
 //images
-import Background from "../../assets/images/hiab/hiabcool03.jpg";
+import Background from "../../assets/images/hiab/hudiksvall01.jpg";
 import InfoIcon from "../../assets/icons/info-red.svg";
 
 const StyledDiv = styled.div`
