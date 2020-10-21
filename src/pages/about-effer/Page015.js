567d0d9262dfe57b99fd0cfed4cd5c84f6b64365
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-21 10:46:30 +0200

    remove links dont used

--- src/pages/about-effer/Page015.js
+++ src/pages/about-effer/Page015.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 // components 
 import Navigation from "../../components/Navigation";
