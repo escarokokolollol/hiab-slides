50aeb1d64d9575d814e809f943fe73cec33266fc
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-21 15:36:33 +0200

    add animation on employees

--- src/pages/about-effer/Page07.js
+++ src/pages/about-effer/Page07.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 // components
@@ -11,15 +11,22 @@ import Background from "../../assets/images/effer/efferpeople01.jpg";
 
 const StyledDiv = styled.div`
   padding: ${props => props.theme.windowPadding};
-  .BlackBoxHeading-wrapper{
+  
+  .black-box{
     width: 90%;
     position: absolute;
     bottom: 60px;
-    div{
-    }
+    opacity: 0;
+    transition: opacity 1s ease;
   }
+
+  .show-black-box{
+    opacity: 1;
+    transition: opacity 2s ease;
+  }
+
   @media ${props => props.theme.thirdBreakpoint}{
-    .BlackBoxHeading-wrapper{
+    .black-box{
       h1{
         font-size:80px;
         line-height:80px;
@@ -29,14 +36,26 @@ const StyledDiv = styled.div`
 `;
 
 const Page07 = () => {
+  
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() =>{
+    console.log('asdaasd');
+    setTimeout(() => {
+      setShowText(true);
+    }, 1000);
+  },[]);
+
   return (
     <div className="page-transition">
       <BackgroundImage image={Background}>
         <StyledDiv>
-          <div className="BlackBoxHeading-wrapper">
+          <div className={`black-box ${showText ? "show-black-box" : ""}`}>
             <BlackBoxHeading title="How we see ourselves" textUnder="is very much a reflection of the image created by our customers’ experiences. In their eyes, we are the company that provides specialised cranes that enable them to conquer the toughest challenges. In the next section we’ll see how this forms the basis of EFFER’s value proposition."/>
           </div>
+            <h1>hide text</h1>
           <Navigation backTo="/about-effer/page05" to="/about-effer/page08"/>
+
         </StyledDiv>
       </BackgroundImage>
     </div>
