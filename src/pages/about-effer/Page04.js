fc550707154784019681fe10cd8917d3ccceb3b8
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-21 10:44:55 +0200

    add youtube icon

--- src/pages/about-effer/Page04.js
+++ src/pages/about-effer/Page04.js
@@ -10,6 +10,7 @@ import BlackBoxHeading from "../../components/BlackBoxHeading";
 import Background from "../../assets/images/effer/effer-big-05.jpg";
 import FacebookIcon from "../../assets/icons/facebook-icon.png";
 import InstagramIcon from "../../assets/icons/instagram-icon.png";
+import YoutubeIcon from "../../assets/icons/youtube-icon.png";
 
 const StyledDiv = styled.div`
   padding: ${props => props.theme.windowPadding};
@@ -45,8 +46,9 @@ const Page04 = () => {
           <div className="BlackBoxHeading-wrapper">
             <BlackBoxHeading titleOver="Today," title="EFFER fans are our best ambassadors." textUnder="Our customers love EFFER because of what they are able to do with our cranes that they can’t accomplish with others. But also because of how we treat them. For if there’s one thing we’ve always been very good at, it’s sitting down with them, listening to what they need and building solutions around those needs.">
               <div className="social-media"> 
-                <img className="facebook-icon" src={FacebookIcon} alt="facebook"/>
-                <img className="instagram-icon"src={InstagramIcon} alt="instagram"/>
+                <img className="icon facebook-icon" src={FacebookIcon} alt="facebook"/>
+                <img className="icon instagram-icon"src={InstagramIcon} alt="instagram"/>
+                <img className="icon youtube-icon"src={YoutubeIcon} alt="youtube"/>
               </div>
             </BlackBoxHeading>
           </div>
