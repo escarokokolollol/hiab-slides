83720b2c86a38e88701d6eb113fdcadad415c70c
Oscar Ek <oscarek@Oscars-iMac.local>
2020-10-22 16:32:03 +0200

    pioneer image

--- src/pages/about-hiab/Page04.js
+++ src/pages/about-hiab/Page04.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 // components
@@ -7,7 +7,7 @@ import BackgroundImage from "../../components/styled/BackgroundImage";
 import BlackBoxHeading from "../../components/BlackBoxHeading";
 
 // images
-import Background from "../../assets/images/hiab/hiab-forest.jpg";
+import Background from "../../assets/images/hiab/pioneer01.png";
 
 const StyledDiv = styled.div`
   padding: ${props => props.theme.windowPadding};
@@ -15,8 +15,14 @@ const StyledDiv = styled.div`
     width: 90%;
     position: absolute;
     bottom: 60px;
+    opacity: 0;
+    transition: opacity 1s ease;
     div{
     }
+  }
+  .show-black-box{
+    opacity: 1;
+    transition: opacity 2s ease;
   }
   @media ${props => props.theme.firstBreakpoint}{
     .BlackBoxHeading-wrapper{
@@ -48,12 +54,20 @@ const StyledDiv = styled.div`
 `;
 
 const Page04 = () => {
+
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() =>{
+    setTimeout(() => {
+      setShowText(true);
+    }, 1000);
+  });
   
   return (
     <div className="page-transition">
       <BackgroundImage image={Background}>
         <StyledDiv>
-          <div className="BlackBoxHeading-wrapper">
+          <div className={`BlackBoxHeading-wrapper ${showText ? "show-black-box" : ""}`}>
             <BlackBoxHeading title="Once a pioneer, always a pioneer" textUnder="Yesterday, being a pioneer was about hydraulics and steel. Today it’s about service and data. Why? Because the two things that our customers care more about now than they’ve ever had to before, are reliability and uptime. And that’s why so many love the HIAB brand."/>
           </div>
           <Navigation backTo="/about-hiab/page03" to="/about-hiab/page05"/>
