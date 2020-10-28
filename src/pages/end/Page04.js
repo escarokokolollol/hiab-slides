354111a0ddce9b31cde3605a608faf3afef08a52
escarokokolollol <oscar_ek21@hotmail.com>
2020-10-28 14:55:51 +0100

    learn more

--- src/pages/end/Page04.js
+++ src/pages/end/Page04.js
@@ -9,8 +9,24 @@ import Navigation from "../../components/Navigation";
 import Background from "../../assets/images/effer_hiab-background.jpg";
 
 const StyledDiv = styled.div`
-  h1{
-    margin: 0;
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .wrapper{
+    margin: 0 auto;
+    width: 70%;
+    h1{
+      margin: 0;
+      font-size: 64px;
+    }
+    p{
+      font-size: 22px;
+      a{
+        color: ${props => props.theme.red};
+      }
+    }
   }
 `;
 
@@ -20,9 +36,11 @@ const Page04 = () => {
     <div className="page-transition">
       <BackgroundImage image={Background}>
         <StyledDiv>
-          <h1>Learn more</h1>
+          <div className="wrapper">
+            <h1>Learn more</h1>
+            <p>To find more materials and information regarding EFFER products, please visit <a href="https://www.effer.com/">effer.com</a> and request access to the reserved area and to our product sharing platform, MySource. Or send a request via e-mail to <a href="mailto:carolina.deganutti@hiab.com">carolina.deganutti@hiab.com</a>.</p>
+          </div>
           <Navigation backTo="/end03" to="/end05" />
-
         </StyledDiv>
       </BackgroundImage>
     </div>
