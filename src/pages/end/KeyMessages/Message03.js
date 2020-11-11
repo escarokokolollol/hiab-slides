b22fb052025b1f3ddd8fd7c8471cfad9ed2d1fb9
Oscar Ek <oscarek@Oscars-iMac.local>
2020-11-11 10:34:21 +0100

    remove a qoute

--- src/pages/end/KeyMessages/Message03.js
+++ src/pages/end/KeyMessages/Message03.js
@@ -92,7 +92,6 @@ const Message03 = () => {
           <div className="text-wrapper">
             <h2 className="brand-name">HIAB <span className="cat">Key messages</span></h2>
             <h1>Once a pioneer. <br/>Always a pioneer.</h1>
-            <p>"Built to last"</p>
             <p>"Inventor of the original loader crane"</p>
             <p>"Built to perform since 1944"</p>
             <p>"Smarter. Faster. Safer."</p>
